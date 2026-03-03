d8ef11ddee38b9f20a05efb5008e0b63830082cf
Daniel Dave M. Umali <134363236+DanielDave2004@users.noreply.github.com>
2026-03-03 11:57:44 +0800

    Implement user addition via POST API
    Added functionality to submit user data via POST API and reload the page upon successful addition.

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const content=document.querySelector("#content");
+const submit=document.querySelector("#add");
 
-window.addEventListener('load', () => {
+//POST API
+submit.addEventListener('click',()=>{
+    let fname=document.querySelector("#fname").value;
+    let lname=document.querySelector("#lname").value;
+    let email=document.querySelector("#email").value;
+    let gender=document.querySelector("#gender").value;
+    let formData={fname,lname,email,gender};
+
+    fetch("https://bscsno3-employees.onrender.com/api/users",{
+        method:'POST',
+        body: JSON.stringify(formData),
+        headers:{
+            "Content-Type":"application/json",
+        },
+    }).catch((error)=>{
+        console.log(error);
+    })
+    alert("User Added Successfully");
+    location.reload();
+});
+
+
+window.addEventListener('load', ()=>{
     getUsers();
 })
 
-function getUsers() {
+
+function getUsers(){
     let html=""
     //FETCH API
-    fetch('https://bscsno3-employees.onrender.com/api/users',{mode: 'cors'})
+    fetch('https://bscsno3-employees.onrender.com/api/users',{mode:'cors'})
     .then(response=>{
         console.log(response);
         return response.json();
     })
-    .then(data=> {
+    .then(data=>{
         console.log(data);
         data.forEach(element=>{
             html+=`<li> ${element.first_name} ${element.last_name}</li>`
         })
+
         content.innerHTML=html;
     })
     .catch(error=>{
         console.log(error);
     })
-
 }
-
